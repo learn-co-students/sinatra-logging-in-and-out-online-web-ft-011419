2e68d761abdc549cafd7f4d263133f76c22cfcd1
Michael Schiumo <mschiumo18@gmail.com>
2019-03-13 11:56:27 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,8 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
+    @user = User.new(username: params["username"], password: params["password"], balance: params["balance"])
+    session[:user_id] = @user.id
 
   end
 
@@ -24,4 +26,3 @@ class ApplicationController < Sinatra::Base
 
 
 end
-
